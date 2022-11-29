5531158141c191301f01882137862f019139f612
yusuf <yusuf4587@gmail.com>
2022-11-29 10:10:20 +0700

    init

---
+++ lib/app/service/logging.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class Loging extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    debugPrint("REQUEST [${options.method}] => PATH: ${options.path}");
+    debugPrint("REQUEST ${options.headers}");
+    debugPrint("REQUEST ${options.data}");
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    debugPrint("RESPONSE ${response.statusCode}");
+    debugPrint("RESPONSE ${response.data}");
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    debugPrint(
+      'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}',
+    );
+  }
+}
